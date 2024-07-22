1233d8f4c48081f4ec6d71e2b045d2dfa3d9c9d9
Noah Salsgiver <Salsgivern2003@gmail.com>
2024-07-22 19:34:27 -0400

    Added missing RAM detection and display.

--- src/SysInsyteMain.java
+++ src/SysInsyteMain.java
@@ -1,11 +1,9 @@
 import javax.swing.*;
+public class SysInsyteMain {
 
-public class SysInsyteMain
-{
     public static void main(String[] args) throws UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         // Assigns Instance of SysInsyte Object to SystemInsightRUn
         SysInsyte SystemInsightRun = new SysInsyte();
-
         // Detects Operating System
         String OS = SystemInsightRun.getOperatingSystem();
         // A little bit of logic to see which mode to use, or whether it's supported at all.
